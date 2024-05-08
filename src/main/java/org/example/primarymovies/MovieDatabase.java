355158ad47c3a353bad68463b4a8b0b393f14c9c
Mehedi Hasan <mehedibuet73@gmail.com>
2024-05-08 07:24:36 +0600

    slightly modified

--- src/main/java/org/example/primarymovies/MovieDatabase.java
+++ src/main/java/org/example/primarymovies/MovieDatabase.java
@@ -9,7 +9,7 @@ public class MovieDatabase {
         movieListing.addMovies("Dhamal", "Sanjay Datt-Arshad", "Comedy", "7-9-2007", "1cr");
         movieListing.addMovies("Monpura", "Chanchal", "Romantic-Realization", "13-2-2009", "5Lacs");
         movieListing.addMovies("Green Card", "Maruf Hayat", "Time Waste", "15-4-2024", "4.5cr");
-        movieListing.addMovies("Avengers:End Game", "RDJ-Chris Evans", "Scifi Action", "26-4-19", "3908cr");
+        movieListing.addMovies("Zavengers:End Game", "RDJ-Chris Evans", "Scifi Action", "26-4-19", "3908cr");
         movieListing.addMovies("Rajkumar", "Shakib Khan", "Romantic-Comedy", "16-5-2024", "50Lacs");
         movieListing.addMovies("Omanush", "Maruf Hayat", "Dark", "15-4-2005", "20Lacs");
         movieListing.addMovies("Priyotoma", "Shakib khan", "Time Waste", "15-4-2024", "4.5cr");
